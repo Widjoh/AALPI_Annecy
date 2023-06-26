245377a9d14f2f2ce7f0832177d6208c19251b5e
pef <fortin.p@hotmail.fr>
2023-06-26 19:51:14 +0200

    feat(route): small adjustements

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Admin from '../views/Admin.vue'
+import Home from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: Admin
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/admin',
+    name: 'admin',
+    component: Admin
+
   }
 ]
 
