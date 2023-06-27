f929b0ae8ed4e573455c0eb849aa9e2e39ce1c36
pef <fortin.p@hotmail.fr>
2023-06-27 16:00:07 +0200

    feat(nav): burger menu

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col">
-    <div class="bg-white flex drop-shadow-md  h-10 sm:justify-between w-[100%] items-center px-6 sm:px-32 py-10 fixed top-0">
+    <div
+        class="bg-white flex drop-shadow-md h-10 sm:justify-between w-[100%] items-center px-6 sm:px-32 py-10 fixed top-0">
       <div class="flex justify-between w-full">
         <router-link to="/">Annecy Assistance</router-link>
         <button class="sm:hidden text-2xl" type="button" @click="toggleMenu()">≣</button>
@@ -13,7 +14,9 @@
       </nav>
     </div>
 
-    <div v-if="displayMenu" class="bg-white fixed top-20 drop-shadow-md	 duration-500 transition-all transition-shadow	shadow-2xl w-full flex flex-col p-6 space-y-4" :class="{ 'hidden': !displayMenu }">
+    <div v-if="displayMenu"
+         class="sm:hidden bg-white fixed top-20 drop-shadow-md duration-500 transition-all transition-shadow	shadow-2xl w-full flex flex-col p-6 space-y-4"
+         :class="{ 'hidden': !displayMenu }">
       <div v-for="link in links" :key="link.path">
         <router-link @click="toggleMenu()" class="hover:text-blue-500" :to="link.path">{{ link.name }}</router-link>
       </div>
@@ -31,9 +34,7 @@ export default {
     const {links} = useLinks();
     const displayMenu = ref(false);
     const toggleMenu = () => {
-      console.log("click")
       displayMenu.value = !displayMenu.value
-      console.log(displayMenu.value)
     }
 
     return {
