1a51964c116c2bf42680d2a1e217c9f254ba4387
pef <fortin.p@hotmail.fr>
2023-07-27 10:57:35 +0200

    feat(media): some improvements and retrieve advice media

--- src/composables/UseDataFetcher.ts
+++ src/composables/UseDataFetcher.ts
@@ -2,10 +2,12 @@ import {ref} from 'vue';
 import axios from "axios";
 
 const homeData = ref(null);
+const adviceMedia = ref(null);
 const loading = ref(false);
 const error = ref(null);
 
 const homeApi = process.env.VUE_APP_ROOT_API + "get/home/data"
+const AdviceMediaApi = process.env.VUE_APP_ROOT_API + "media/get/images/advice"
 
 export function useDataFetcher() {
 
@@ -17,11 +19,32 @@ export function useDataFetcher() {
             axios.get(homeApi)
                 .then((response) => {
                     homeData.value = response.data; // 👈 get just results
-                });
+                }).catch(function (error) {
+                console.log(error.toJSON());
+            });
+        }
+
+    }
+
+    async function fetchAdviceMedia() {
+        if (adviceMedia.value) {
+            return adviceMedia.value;
+        } else {
+            axios.get(AdviceMediaApi, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`
+                }
+            })
+                .then((response) => {
+                    adviceMedia.value = response.data; // 👈 get just results
+                }).catch(function (error) {
+                console.log("ERROR")
+                console.log(error.toJSON());
+            });
         }
 
     }
 
 
-    return {homeData, loading, error, fetchHomeData,};
+    return {homeData, loading, error, fetchHomeData, fetchAdviceMedia, adviceMedia};
 }
--- src/views/Admin/Images.vue
+++ src/views/Admin/Images.vue
@@ -1,101 +1,105 @@
 <template>
   <div class="flex justify-center pb-10">
-    <div class="flex flex-col md:flex-row justify-center w-full items-center md:items-baseline md:space-x-16 md:ml-[190px] lg:ml-0">
-    <div class="md:fixed md:left-0 bg-slate-600 w-full md:w-[25%] lg:sm:w-[15%] md:h-full p-6 flex sm:flex-col space-y-4 flex flex-wrap ">
-      <button @click="showDiv = 'uploader'"
-              class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
-       Images
-      </button>
-      <button @click="showDiv = 'conseil'"
-              class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
-        Conseil
-      </button>
-      <button @click="showDiv = 'Maintenance'"
-              class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
-        Maintenance
-      </button>
-      <button @click="showDiv = 'Support'"
-              class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
-        Support
-      </button>
-      <button @click="showDiv = 'Installation'"
-              class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
-        Installation
-      </button>
-    </div>
-    <div v-if="showDiv === 'uploader'" class="upload bg-slate-300 rounded p-6 w-[300px] sm:w-[400px] mt-6 ">
-      <div v-if="files.length" class="grid grid-cols-3">
-        <div v-for="file in files" :key="file.id">
-          <div>
-            <img v-if="file.thumb" :src="file.thumb" alt="file.name" class="object-cover h-48 w-96"/>
+    <div
+        class="flex flex-col md:flex-row justify-center w-full items-center md:items-baseline md:space-x-16 md:ml-[190px] lg:ml-0">
+      <div
+          class="md:fixed md:left-0 bg-slate-600 w-full md:w-[25%] lg:sm:w-[15%] md:h-full p-6 flex sm:flex-col space-y-4 flex flex-wrap ">
+        <button @click="showDiv = 'uploader'"
+                class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
+          Images
+        </button>
+        <button @click="showDiv = 'conseil'"
+                class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
+          Conseil
+        </button>
+        <button @click="showDiv = 'Maintenance'"
+                class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
+          Maintenance
+        </button>
+        <button @click="showDiv = 'Support'"
+                class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
+          Support
+        </button>
+        <button @click="showDiv = 'Installation'"
+                class="bg-transparent w-full hover:bg-blue-500 text-slate-100 font-semibold hover:text-white p-2 border border-slate-500 hover:border-transparent rounded">
+          Installation
+        </button>
+      </div>
+      <div v-if="showDiv === 'uploader'" class="upload bg-slate-300 rounded p-6 w-[300px] sm:w-[400px] mt-6 ">
+        <div v-if="files.length" class="grid grid-cols-3">
+          <div v-for="file in files" :key="file.id">
+            <div>
+              <img v-if="file.thumb" :src="file.thumb" alt="file.name" class="object-cover h-48 w-96"/>
+            </div>
+          </div>
+        </div>
+        <ul v-else>
+          <td colspan="7">
+            <div class="text-center p-5">
+              <h4>Déposez des fichiers pour les télécharger<br/>ou</h4>
+              <label for="file" class="btn btn-lg btn-primary cursor-pointer">sélectionner des fichiers</label><br/>
+              <span>jpg / jpeg / png</span>
+
+            </div>
+          </td>
+        </ul>
+        <div class="flex justify-center space-y-4 ">
+          <file-upload
+              class="btn btn-primary"
+              :post-action="api"
+              :multiple="true"
+              :data="category"
+              :headers="{Authorization: token}"
+              :extensions="extensions"
+              :accept="accept"
+              v-model="files"
+              @input-filter="inputFilter"
+              @input-file="inputFile"
+              ref="upload">
+          </file-upload>
+          <div class="flex flex-col space-y-4">
+            <div>
+              <label for="category" class="text-slate-700 font-semibold">Sélectionne la catégorie</label>
+              <select v-model="selectedCategory" id="category" required
+                      class="block w-full mt-1 p-2 rounded-md border border-slate-500 focus:outline-none focus:border-blue-500">
+                <option value="advice">Conseil</option>
+                <option value="maintenance">Maintenance</option>
+                <option value="support">Support</option>
+                <option value="install">Installation</option>
+                <!-- Add more options as needed -->
+              </select>
+            </div>
+
+            <button type="button"
+                    class="bg-transparent hover:bg-blue-500 text-slate-500 font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded"
+                    :class=" selectedCategory === ''  ? 'pointer-events-none' : 'cursor-pointer'  "
+                    v-if="!$refs.upload || !$refs.upload.active"
+                    @click.prevent="$refs.upload.active = true">
+              Upload
+            </button>
           </div>
         </div>
       </div>
-      <ul v-else>
-        <td colspan="7">
-          <div class="text-center p-5">
-            <h4>Déposez des fichiers pour les télécharger<br/>ou</h4>
-            <label for="file" class="btn btn-lg btn-primary cursor-pointer">sélectionner des fichiers</label><br/>
-            <span>jpg / jpeg / png</span>
-
-          </div>
-        </td>
-      </ul>
-      <div class="flex justify-center space-y-4 ">
-        <file-upload
-            class="btn btn-primary"
-            :post-action="api"
-            :multiple="true"
-            :data="categorie"
-            :headers="{Authorization: token}"
-            :extensions="extensions"
-            :accept="accept"
-            v-model="files"
-            @input-filter="inputFilter"
-            @input-file="inputFile"
-            ref="upload">
-        </file-upload>
-        <div class="flex flex-col space-y-4">
-          <div>
-            <label for="category" class="text-slate-700 font-semibold">Sélectionne la catégorie</label>
-            <select v-model="selectedCategory" id="category" required
-                    class="block w-full mt-1 p-2 rounded-md border border-slate-500 focus:outline-none focus:border-blue-500">
-              <option value="advice">Conseil</option>
-              <option value="maintenance">Maintenance</option>
-              <option value="support">Support</option>
-              <option value="install">Installation</option>
-              <!-- Add more options as needed -->
-            </select>
-          </div>
-
-          <button type="button"
-                  class="bg-transparent hover:bg-blue-500 text-slate-500 font-semibold hover:text-white py-2 px-4 border border-slate-500 hover:border-transparent rounded"
-                  :class=" selectedCategory === ''  ? 'pointer-events-none' : 'cursor-pointer'  "
-                  v-if="!$refs.upload || !$refs.upload.active"
-                  @click.prevent="$refs.upload.active = true">
-            Upload
-          </button>
-        </div>
+      <div v-if="showDiv === 'conseil'" class="support">
+        <span>Conseil bitch</span>
+      </div>
+      <div v-if="showDiv === 'Maintenance'" class="support">
+        <span>Maintenance bitch</span>
+      </div>
+      <div v-if="showDiv === 'Support'" class="support">
+        <span>Support bitch</span>
+      </div>
+      <div v-if="showDiv === 'Installation'" class="support">
+        <span>Installation bitch</span>
       </div>
     </div>
-    <div v-if="showDiv === 'conseil'" class="support">
-      <span>Conseil bitch</span>
-    </div>
-    <div v-if="showDiv === 'Maintenance'" class="support">
-      <span>Maintenance bitch</span>
-    </div>
-    <div v-if="showDiv === 'Support'" class="support">
-      <span>Support bitch</span>
-    </div>
-    <div v-if="showDiv === 'Installation'" class="support">
-      <span>Installation bitch</span>
-    </div>
-  </div>
   </div>
 </template>
 <script>
 import FileUpload from 'vue-upload-component'
 import {notify} from "@kyvg/vue3-notification";
+import {useDataFetcher} from "@/composables/UseDataFetcher.ts";
+import {onMounted, ref, watch} from "vue";
 
 export default {
   name: 'AdminImages',
@@ -106,18 +110,41 @@ export default {
       accept: 'image/png,image/jpeg,image/jpeg',
       extensions: 'jpg,jpeg,png',
       api: process.env.VUE_APP_ROOT_API + 'media/add/images',
-      selectedCategory: null,
       showDiv: 'uploader',
     }
   },
+  setup() {
+    const {adviceMedia, loading, error, fetchAdviceMedia} = useDataFetcher();
+    const selectedCategory = ref('advice')
+    const category = ref(null)
+    onMounted(() => {
+      category.value = {'categorie': "advice"}
+      fetchAdviceMedia();
+      console.log(adviceMedia)
+    });
+
+
+    watch(selectedCategory, (newValue, oldValue) => {
+          if (newValue !== oldValue) {
+            selectedCategory.value = newValue
+            category.value = {'categorie': selectedCategory.value}
+          }
+        }
+    );
+
+    return {
+      adviceMedia,
+      loading,
+      error,
+      selectedCategory,
+      category
+    };
+  },
+
   computed: {
     token() {
       return `Bearer ${localStorage.getItem('token')}`
     },
-    categorie() {
-      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-      return {'categorie': this.selectedCategory}
-    }
 
   },
   methods: {
@@ -188,7 +215,6 @@ export default {
             text: "Success",
           });
           this.files = [];
-          this.selectedCategory = "";
         }
       }
 
@@ -212,9 +238,7 @@ export default {
       }
     },
   },
-  setup() {
-    return {};
-  },
+
 
 }
 
