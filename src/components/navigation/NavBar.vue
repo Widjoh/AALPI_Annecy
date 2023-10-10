eb6e62887d7a5ea04c3bf624b0d0086dd386031f
pef <fortin.p@hotmail.fr>
2023-10-10 20:54:01 +0200

    feat(all): change icon and colors

--- src/components/navigation/NavBar.vue
+++ src/components/navigation/NavBar.vue
@@ -9,12 +9,12 @@ const { links } = useLinks();
 
 </script>
 <template>
-  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+  <Disclosure as="nav" class="bg-[#2A358C]" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
-          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
             <span class="absolute -inset-0.5" />
             <span class="sr-only">Open main menu</span>
             <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
@@ -22,12 +22,13 @@ const { links } = useLinks();
           </DisclosureButton>
         </div>
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-          <div class="flex flex-shrink-0 items-center">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+          <div class="flex flex-shrink-0 items-center jus">
+            <img class="h-20" :src="require(`@/assets/svg/LogoAALPI_Blanc.svg`)" alt="AALPI LOGO" />
+
           </div>
-          <div class="hidden sm:ml-6 sm:block">
+          <div class="hidden sm:ml-6 sm:block pt-6">
             <div class="flex space-x-4">
-              <a v-for="item in links" :key="item.name" :href="item.href" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" :aria-current="item.current ? 'page' : undefined" >{{ item.name }}</a>
+              <a v-for="item in links" :key="item.name" :href="item.href" class="text-gray-300 hover:bg-gray-700/10 hover:text-white rounded-md px-3 py-2 text-sm font-medium" :aria-current="item.current ? 'page' : undefined" >{{ item.name }}</a>
             </div>
           </div>
         </div>
